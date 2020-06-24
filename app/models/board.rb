fedf6cb9f1c4632eaa5fbcfacd613067842ca991
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-06-24 19:47:00 +0900

    install gem acts-as-taggable-on

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint           not null, primary key
+#  content    :string(255)      default(""), not null
+#  title      :string(255)      default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Board < ApplicationRecord
+  acts_as_taggable_on :tags
 end
--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint           not null, primary key
+#  content    :string(255)      default(""), not null
+#  title      :string(255)      default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class BoardTest < ActiveSupport::TestCase
