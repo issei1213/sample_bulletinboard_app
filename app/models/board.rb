1e36cc4457d77b09159c3237cff4142eda9bc0be
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-06-25 23:19:52 +0900

    boards table add foreign key

--- app/models/board.rb
+++ app/models/board.rb
@@ -7,8 +7,18 @@
 #  title      :string(255)      default(""), not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_boards_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 class Board < ApplicationRecord
+  belongs_to :user
   acts_as_taggable_on :tags
 
   default_scope -> { order(created_at: :desc) }
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_104734) do
+ActiveRecord::Schema.define(version: 2020_06_25_123236) do
 
   create_table "boards", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "title", default: "", null: false, collation: "utf8mb4_general_ci"
     t.string "content", default: "", null: false, collation: "utf8mb4_general_ci"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
   create_table "taggings", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
@@ -59,5 +61,6 @@ ActiveRecord::Schema.define(version: 2020_06_25_104734) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "boards", "users"
   add_foreign_key "taggings", "tags"
 end
